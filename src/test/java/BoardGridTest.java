6412b3147e3966f3c39c49e160574f760d526d05
Charlie Conneely <g00348887@gmit.ie>
2021-05-30 15:34:04 +0100

    Imported Mockitio for mocks/stubs.

--- src/test/java/BoardGridTest.java
+++ src/test/java/BoardGridTest.java
@@ -3,18 +3,31 @@ import Server.GameManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class BoardGridTest {
 
+    private BoardGrid boardGrid;
+    @Mock
+    private GameManager gameManager;
+
+    @BeforeEach
+    void setUp() {
+        boardGrid = new BoardGrid(gameManager);
+        // Initialise mocks
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    @DisplayName("0 = 'x'")
-    void assignsCorrectCharacter() {
-        BoardGrid boardGrid = new BoardGrid(new GameManager());
-        assertEquals("x", boardGrid.classifySymbol(0), "0 should return 'x'");
+    @DisplayName("'Column is full' message displays")
+    void makeMoveOnFullColumn() {
+        boardGrid.initializeMatrix();
     }
 
     @ParameterizedTest(name = "{0} = {1}")
